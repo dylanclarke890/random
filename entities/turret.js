e4f9e1f5b15010c6bd06a7530330810eae5aaa27
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-11 21:00:50 +0000

    added basic cannon entity

--- entities/turret.js
+++ entities/turret.js
@@ -1,6 +1,38 @@
 import { Entity } from "../canvas-game-engine/modules/core/entity.js";
 import { Register } from "../canvas-game-engine/modules/core/register.js";
 
-export class TurretBase extends Entity {}
+class TowerBase extends Entity {
+  constructor(opts) {
+    super(opts);
+    this.createAnimationSheet("assets/turrets/Tower.png");
+  }
+}
 
-Register.entityTypes(TurretBase);
+class BaseTurret extends Entity {
+  constructor(opts) {
+    super(opts);
+    this.towerBase = new TowerBase(opts);
+  }
+
+  draw() {
+    super.draw();
+    this.towerBase.draw();
+  }
+}
+
+export class Cannon_1 extends BaseTurret {
+  constructor(opts) {
+    super(opts);
+    this.createAnimationSheet("assets/turrets/Cannon.png");
+  }
+
+  draw() {
+    super.draw();
+  }
+
+  update() {
+    super.update();
+  }
+}
+
+Register.entityTypes(TowerBase, Cannon_1);
