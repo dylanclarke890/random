0f89b7e01b3a7fcc87bc2ed854502e771bcb02ee
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-12 18:20:27 +0000

    working on enemy pathfinding

--- entities/enemy.js
+++ entities/enemy.js
@@ -9,11 +9,14 @@ class Enemy extends Entity {
   static framesToSecs = (frames) => (1 / 60) * frames;
 
   vel = { x: 10, y: 0 };
+  /** @type {number[][]} */
+  targetPath;
+  currentPoint = 0;
 
   constructor({ spritesheetName, ...opts }) {
     super(opts);
-    this.size = { x: 66, y: 58 };
-    this.createAnimationSheet(`assets/spritesheets/${spritesheetName}.png`, this.size);
+    this.size = { x: 32, y: 64 };
+    this.createAnimationSheet(`assets/spritesheets/${spritesheetName}.png`, { x: 66, y: 58 });
 
     const defaultDuration = Enemy.framesToSecs(3);
     this.addAnim("attack", defaultDuration, Enemy.get_sequence(0), false);
@@ -24,6 +27,18 @@ class Enemy extends Entity {
     this.addAnim("run", defaultDuration, Enemy.get_sequence(100), false);
     this.addAnim("walk", defaultDuration, Enemy.get_sequence(120), false);
     this.currentAnim = this.anims.walk;
+  }
+
+  setDestination(path) {
+    this.targetPath = [...path];
+  }
+
+  update() {
+    super.update();
+    if (this.targetPath) {
+      const [x, y] = this.targetPath[this.currentPoint];
+      console.log(x, y);
+    }
   }
 }
 
--- game.js
+++ game.js
@@ -1,23 +1,68 @@
 import { Game } from "./canvas-game-engine/modules/core/game.js";
 import { Input } from "./canvas-game-engine/modules/core/input.js";
-import { Cannon, TurretSelector } from "./entities/entities.js";
+import { Algorithm, HeuristicType } from "./canvas-game-engine/modules/pathfinding/constants.js";
+import { Grid } from "./canvas-game-engine/modules/pathfinding/data-structures.js";
+import { PathFinder } from "./canvas-game-engine/modules/pathfinding/pathfinder.js";
+import { compressPath, smoothenPath } from "./canvas-game-engine/modules/pathfinding/utils.js";
+import { Cannon, Enemy_Pitchfork, TurretSelector } from "./entities/entities.js";
 import { baseLevel } from "./levels/baseLevel.js";
 
 export class TowerDefenseGame extends Game {
   static MAP_TILE_SIZE = 32;
   /** @type {TurretSelector}  */
   turretSelector;
+  /** @type {PathFinder}  */
+  pathfinder;
+  /** @type {number[][]}  */
+  path;
 
   constructor(opts) {
     super(opts);
     this.loadLevel(baseLevel);
     this.input.bind(Input.KEY.MOUSE1, "place_cannon");
+    this.pathfinder = new PathFinder({
+      algorithm: Algorithm.AStar,
+      heuristic: HeuristicType.Octile,
+      allowDiagonal: true,
+    });
+    const grid = new Grid({ matrix: this.collisionMap.data });
+    this.pathMatrix = compressPath(this.pathfinder.findPath(0, 3, 0, 16, grid));
+    console.log(this.pathMatrix);
+    this.path = this.pathMatrix.map(([x, y]) => [
+      x * TowerDefenseGame.MAP_TILE_SIZE,
+      y * TowerDefenseGame.MAP_TILE_SIZE,
+    ]);
+    this.updateEnemiesWithPath();
+
     this.turretSelector = this.spawnEntity(TurretSelector, this.input.mouse.x, this.input.mouse.x);
     this.turretSelector.setSelected(Cannon);
   }
 
+  updateEnemiesWithPath() {
+    const enemies = this.getEntitiesByType(Enemy_Pitchfork);
+    for (let i = 0; i < enemies.length; i++) {
+      const enemy = enemies[i];
+      enemy.setDestination(this.path);
+    }
+  }
+
   draw() {
     super.draw();
+    this.drawPath();
+  }
+
+  drawPath() {
+    const { ctx } = this.system;
+    const start = this.path[0];
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "orange";
+    ctx.beginPath();
+    ctx.moveTo(start[0], start[1]);
+    for (let i = 1; i < this.path.length; i++) {
+      const [x, y] = this.path[i];
+      ctx.lineTo(x, y);
+    }
+    ctx.stroke();
   }
 
   update() {
