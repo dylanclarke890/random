d1875ac8056ba6d60fd8bcf75e343ef48a5cc55d
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-12 05:02:40 +0000

    added logic to check for already placed turrets. fixed an error with checking map tiles

--- entities/turret.js
+++ entities/turret.js
@@ -61,6 +61,8 @@ export class Cannon extends Entity {
 export class TurretSelector extends Entity {
   /** @type {Entity} */
   selected;
+  /** @type {typeof Entity} */
+  turretType;
   isValidPosition = true;
 
   collides = Entity.COLLIDES.LITE;
@@ -82,40 +84,46 @@ export class TurretSelector extends Entity {
 
   setPosition({ x, y }) {
     if (!this.selected) return;
+
     // Center the selected turret on the mouse position
     x = x - this.selected.size.x / 2;
     y = y - this.selected.size.y / 2;
 
-    // Snap to grid
+    // Snap the selected turret to the grid to grid
     x = this.snapToGrip(x);
     y = this.snapToGrip(y);
 
     this.selected.pos.x = x;
     this.selected.pos.y = y;
 
-    // Check if the selector is in a valid area by comparing its position with the map data's
+    // Check if the selected turret is in a valid area by comparing its position with the map data's
     x /= RandomGame.MAP_TILE_SIZE;
     y /= RandomGame.MAP_TILE_SIZE;
-
     this.isValidPosition = true;
 
-    if (x < 0 || y < 0 || x > this.mapWidth || y > this.mapHeight) {
-      this.isValidPosition = false;
-      return;
-    }
-    x = Math.abs(x);
-    y = Math.abs(y);
-
+    // Check if any of the positions of the selected turret are on invalid tiles
     const positions = [
-      this.buildPositions[y][x],
-      this.buildPositions[y][x + 1],
-      this.buildPositions[y + 1][x],
-      this.buildPositions[y + 1][x + 1],
+      this.buildPositions[y]?.[x],
+      this.buildPositions[y]?.[x + 1],
+      this.buildPositions[y + 1]?.[x],
+      this.buildPositions[y + 1]?.[x + 1],
     ];
     this.isValidPosition = !positions.some((pos) => pos !== 1);
+    if (!this.isValidPosition) return;
+
+    // Check if there are any turrets where the selected turret is about to be placed
+    const currentlyPlacedTurrets = this.game.getEntitiesByType(this.turretType);
+    for (let i = 0; i < currentlyPlacedTurrets.length; i++) {
+      const current = currentlyPlacedTurrets[i];
+      if (this.selected.touches(current)) {
+        this.isValidPosition = false;
+        return;
+      }
+    }
   }
 
   setSelected(turretType) {
+    this.turretType = turretType;
     this.selected = new turretType({ x: this.pos.x, y: this.pos.y, game: this.game });
     this.selected.setAlpha(0.5);
   }
