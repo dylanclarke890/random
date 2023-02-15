7994b28a15406cb058ee6271b5ca12c981ac8172
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-15 21:06:40 +0000

    renamed enemy entities file

--- entities/entities.js
+++ entities/entities.js
@@ -1,4 +1,4 @@
-export { Enemy_Pitchfork } from "./enemy.js";
+export { Enemy_Pitchfork } from "./enemies.js";
 export { Cannon, RPG, MachineGun } from "./turrets.js";
 export { CannonBullet, MGBullet, Missile } from "./projectiles.js";
 export { TurretSelector } from "./turret-selector.js";
