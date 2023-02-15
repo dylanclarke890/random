a5084442b22d363137f9f127e3dc97761bc7a911
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-15 21:43:13 +0000

    added earthquake entity

--- entities/earthquake.js
+++ entities/earthquake.js
@@ -1,58 +1,45 @@
-/*
-This entity shakes the screen when its triggeredBy() method is called - usually
-through an EntityTrigger entity.
+import { Entity } from "../canvas-game-engine/modules/core/entity.js";
+import { Timer } from "../canvas-game-engine/modules/lib/timer.js";
 
+/**
+ * This entity shakes the screen when its triggeredBy() method is called - usually
+ * through an EntityTrigger entity.
+ *
+ * Keys for LevelEditor:
+ * - strength:
+ * max amount of screen movement in pixels
+ * default: 8
+ * - duration:
+ * duration of the screen shaking in seconds
+ * default: 1
+ */
+export class EntityEarthquake extends Entity {
+  _levelEditorDrawBox = true;
+  _levelEditorBoxColor = "rgba(80, 130, 170, 0.7)";
 
-Keys for Weltmeister:
+  size = { x: 8, y: 8 };
 
-strength 
-	max amount of screen movement in pixels
-	default: 8
+  duration = 1;
+  strength = 8;
+  quakeTimer = null;
 
-duration 
-	duration of the screen shaking in seconds
-	default: 1
-*/
+  constructor(opts) {
+    super(opts);
+    this.quakeTimer = new Timer();
+  }
 
-ig.module(
-	'game.entities.earthquake'
-)
-.requires(
-	'impact.entity'
-)
-.defines(function(){
+  triggeredBy() {
+    this.quakeTimer.set(this.duration);
+  }
 
-EntityEarthquake = ig.Entity.extend({
-	_wmDrawBox: true,
-	_wmBoxColor: 'rgba(80, 130, 170, 0.7)',
-	
-	size: {x: 8, y: 8},
-	
-	duration: 1,
-	strength: 8,
-	quakeTimer: null,	
-	
-	init: function( x, y, settings ) {
-		this.quakeTimer = new ig.Timer();
-		this.parent( x, y, settings );
-	},	
-	
-	
-	triggeredBy: function( entity, trigger ) {
-		this.quakeTimer.set( this.duration );
-	},
-	
-	
-	update: function() {
-		var delta = this.quakeTimer.delta();
-		if( delta < -0.1 ) {
-			var s = this.strength * Math.pow( -delta / this.duration, 2 );
-			if( s > 0.5 ) {
-				ig.game.screen.x += Math.random().map( 0, 1, -s, s );
-				ig.game.screen.y += Math.random().map( 0, 1, -s, s );
-			}
-		}
-	}
-});
+  update() {
+    const delta = this.quakeTimer.delta();
+    if (delta >= -0.1) return;
 
-});
+    const s = this.strength * Math.pow(-delta / this.duration, 2);
+    if (s <= 0.5) return;
+
+    this.game.screen.x += Math.random().map(0, 1, -s, s);
+    this.game.screen.y += Math.random().map(0, 1, -s, s);
+  }
+}
