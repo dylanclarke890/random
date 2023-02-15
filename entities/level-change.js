33b8c5e11eb9a236ec6e73b54788ff8db7c8f957
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-15 21:50:54 +0000

    added level-change entity

---
+++ entities/level-change.js
@@ -0,0 +1,26 @@
+/**
+ * This entity calls ig.game.loadLevel() when its triggeredBy() method is called -
+ * usually through an EntityTrigger entity.
+ *
+ * Keys for LevelEditor:
+ *
+ * - level:
+ * Name of the level to load. E.g. "LevelTest1" or just "test1" will load the
+ * 'LevelTest1' level.
+ */
+
+import { Entity } from "../canvas-game-engine/modules/core/entity.js";
+
+export class EntityLevelChange extends Entity {
+  _levelEditorDrawBox = true;
+  _levelEditorBoxColor = "rgba(0, 0, 255, 0.7)";
+
+  size = { x: 8, y: 8 };
+  level = null;
+
+  triggeredBy() {
+    if (this.level) this.game.loadLevelDeferred(this.level);
+  }
+
+  update() {}
+}
