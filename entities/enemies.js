c92402f46648f5eef9b34267b70a7defe0d29fee
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-17 00:15:10 +0000

    rewrote enemy animations to use new interval notation to specify their sequence range

--- entities/enemies.js
+++ entities/enemies.js
@@ -2,12 +2,6 @@ import { Register } from "../canvas-game-engine/modules/core/register.js";
 import { EntityMover } from "../canvas-game-engine/modules/lib/entities/mover.js";
 
 export class Enemy extends EntityMover {
-  static get_sequence(start) {
-    start ??= 0;
-    return Array.from({ length: 20 }, (_v, i) => i + start);
-  }
-  static framesToSecs = (frames) => (1 / 60) * frames;
-
   maxHealth = 20;
   health = this.maxHealth;
   offset = { x: 32, y: 32 };
@@ -18,14 +12,14 @@ export class Enemy extends EntityMover {
     this.speed = 100;
     this.createAnimationSheet(`assets/spritesheets/${spritesheetName}.png`, { x: 66, y: 58 });
 
-    const defaultDuration = Enemy.framesToSecs(3);
-    this.addAnim("attack", defaultDuration, Enemy.get_sequence(0), false);
-    this.addAnim("die", defaultDuration, Enemy.get_sequence(20), false);
-    this.addAnim("hurt", defaultDuration, Enemy.get_sequence(40), false);
-    this.addAnim("idle", defaultDuration, Enemy.get_sequence(60), false);
-    this.addAnim("jump", defaultDuration, Enemy.get_sequence(80), false);
-    this.addAnim("run", defaultDuration, Enemy.get_sequence(100), false);
-    this.addAnim("walk", defaultDuration, Enemy.get_sequence(120), false);
+    const threeFrames = (1 / 60) * 3;
+    this.addAnim("attack", threeFrames, "[0..19]", false);
+    this.addAnim("die", threeFrames, "[20..39]", false);
+    this.addAnim("hurt", threeFrames, "[40..59]", false);
+    this.addAnim("idle", threeFrames, "[60..79]", false);
+    this.addAnim("jump", threeFrames, "[80..99]", false);
+    this.addAnim("run", threeFrames, "[100..119]", false);
+    this.addAnim("walk", threeFrames, "[120..139]", false);
     this.currentAnim = this.anims.walk;
   }
 
