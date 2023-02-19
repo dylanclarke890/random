5292d66245bf829115ecdd33f7ac6f70921a5666
dylanclarke890 <dylanclarke890@gmail.com>
2023-02-19 04:58:42 +0000

    started work on js

--- krystalizor/krystalizor.js
+++ krystalizor/krystalizor.js
@@ -1,10 +1,38 @@
 import { GameLoop } from "../canvas-game-engine/modules/core/loop.js";
 
-export class Krystalizor {
+class System {
   constructor() {
-    this.loop = new GameLoop({ runner: this });
+    this.tick = 0;
+    this.canvas = document.querySelector("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    const header = document.querySelector("header");
+    const panels = document.querySelector("#panels");
+    this.canvas.height = window.innerHeight - header.offsetHeight;
+    this.canvas.width = window.innerWidth - panels.offsetWidth;
+  }
+}
+
+class Canvas {
+  constructor(system) {
+    this.system = system;
+  }
+
+  draw() {
+    const ctx = this.system.ctx;
+    ctx.fillStyle = "orange";
+    ctx.clearRect(0, 0, 200, 200);
+    ctx.fillRect(0, 0, 200, 200);
   }
 
   update() {}
-  draw() {}
+}
+
+export class Krystalizor {
+  constructor() {
+    this.system = new System();
+    this.canvas = new Canvas(this.system);
+    this.game = this.canvas; // for game loop
+    this.loop = new GameLoop({ runner: this });
+    this.loop.start();
+  }
 }
--- krystalizor/main.js
+++ krystalizor/main.js
@@ -1,3 +1,12 @@
 import { Krystalizor } from "./krystalizor.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const visibilityIcons = document.querySelectorAll(".layer__visibility");
+  for (let i = 0; i < visibilityIcons.length; i++) {
+    const icon = visibilityIcons[i];
+    icon.addEventListener("click", () => {
+      icon.dataset.checked = icon.dataset.checked === "true" ? "false" : "true";
+    });
+  }
+});
 new Krystalizor();
